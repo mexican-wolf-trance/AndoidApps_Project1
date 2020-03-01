1d44b968f9a085fcf40375be63aa4a75c5690498
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-03-01 12:30:40 -0600

    Changing the menu screen somewhat

--- app/src/main/java/com/example/studentattendance/DetailsActivity.kt
+++ app/src/main/java/com/example/studentattendance/DetailsActivity.kt
@@ -1,7 +1,14 @@
 package com.example.studentattendance
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DetailsActivity: AppCompatActivity() {
-
+class DetailsActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?)
+    {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.courses_layout)
+    }
 }
